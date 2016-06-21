f7b0c7ac82bed0edbed3fc611e30e2913534c4f8
Newomatic Corp <newomatic@gmail.com>
2016-06-21 16:51:13 -0400

    working version of the app with suggestions

--- index.ios.js
+++ index.ios.js
@@ -20,56 +20,70 @@ let realm;
 class Realm13 extends Component {
   constructor(props) {
     super(props);
-
-    RNFS.unlink(RNFS.DocumentDirectoryPath+"/default.realm");
-     RNFS.unlink(RNFS.DocumentDirectoryPath+"/default.realm.lock");
-     RNFS.unlink(RNFS.DocumentDirectoryPath+"/default.realm.management");
-
-    // RNFS.unlink(RNFS.DocumentDirectoryPath+"/newDefault.realm");
-     RNFS.moveFile(RNFS.MainBundlePath+"/default.realm", RNFS.DocumentDirectoryPath+"/default.realm")
-      .then((success) => {
-        console.log('FILE WRITTEN!');
-        realm = new Realm({
-          // path: 'newDefault.realm',
-          schema: [{name: 'Dog', properties: {name: 'string'}}]
-         });
-         // console.log(realm.path, RNFS.DocumentDirectoryPath);
-         return realm
-      })
-      .catch((err) => {
-        console.log(err.message);
-      });
-  
-    this.state = {};
+  }
+  componentWillMount(){
+   // RNFS.unlink(RNFS.DocumentDirectoryPath+"/default.realm");
+   // RNFS.unlink(RNFS.DocumentDirectoryPath+"/default.realm.lock");
+   // RNFS.unlink(RNFS.DocumentDirectoryPath+"/default.realm.management");
+    // RNFS.exists(RNFS.DocumentDirectoryPath+"/default.realm.lock")
+    //   .then(()=>{
+        RNFS.unlink(RNFS.DocumentDirectoryPath+"/default.realm.lock");
+      // });
+    // RNFS.exists(RNFS.DocumentDirectoryPath+"/default.realm.management")
+    //   .then(()=>{
+        RNFS.unlink(RNFS.DocumentDirectoryPath+"/default.realm.management");
+      // });
+    // RNFS.exists(RNFS.DocumentDirectoryPath+"/default.realm")
+    //   .then(()=>{
+        RNFS.unlink(RNFS.DocumentDirectoryPath+"/default.realm");
+        RNFS.moveFile(RNFS.MainBundlePath+"/default.realm", RNFS.DocumentDirectoryPath+"/default.realm")
+        .then((success) => {
+          console.log('FILE WRITTEN!');
+           // console.log(realm.path, RNFS.DocumentDirectoryPath);
+           // return realm
+           realm = new Realm({
+            // path: 'newDefault.realm',
+            schema: [{name: 'Dog', properties: {name: 'string'}}]
+           });
+           this.setState({realm: realm});
+           return realm;
+        })
+        .catch((err) => {
+          console.log(err.message);
+        });
+      // });
+    this.state = {realm: realm};
+  }
+  renderNewComponent(){
+    console.log(this.state.realm.path, RNFS.DocumentDirectoryPath);
+    return (
+      <Text style={styles.welcome}>
+        Count of Dogs in Realm: {this.state.realm.objects('Dog').length};
+      </Text>
+      );
   }
    render() {
 
      // Realm.defaultPath = 'newDefault.realm';
 
-     realm = new Realm({
-      // path: 'newDefault.realm',
-      schema: [{name: 'Dog', properties: {name: 'string'}}]
-     });
-     console.log(realm.path, RNFS.DocumentDirectoryPath);
+     let renderComponent = this.state.realm != undefined ? this.renderNewComponent() : null;
 
-     // let dogs = realm.objects('Dog');
+     // let dogs = realmObj.objects('Dog');
      // let dog = dogs.slice(0, 1)[0];
      // console.log(dog);
 
 
-     // realm.write(() => {
-     //   realm.create('Dog', {name: 'Rex'});
+     // realmObj.write(() => {
+     //   realmObj.create('Dog', {name: 'Rex'});
      // });
 
-     // realm.write(() => {
+     // realmObj.write(() => {
      //  dog.name = "John Snow";
      // });
 
      return (
        <View style={styles.container}>
-         <Text style={styles.welcome}>
-           Count of Dogs in Realm: {realm.objects('Dog').length}
-         </Text>
+         {renderComponent}
        </View>
      );
    }
